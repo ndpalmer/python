224694c60708c853525c8770006e1015cc2f3957
ndpalmer <ndpalmer@gmail.com>
2022-05-18 09:18:07 +1000

    Formatting

--- bandwidth_usage.py
+++ bandwidth_usage.py
@@ -2,21 +2,17 @@ import psutil
 from time import sleep
 
 def __main__():
-
     get_bandwidth       = psutil.net_io_counters()
     initial_total       = get_bandwidth.bytes_sent + get_bandwidth.bytes_recv
 
-    while True:
-        
+    while True:        
         get_bandwidth   = psutil.net_io_counters()
         bandwidth_used  = get_bandwidth.bytes_sent + get_bandwidth.bytes_recv - initial_total
 
         print_total(bandwidth_used)
         sleep(1)
 
-
 def format_bytes(size):
-
     power = 2**10
     n = 0
     power_labels = {0 : '', 1: 'kilo', 2: 'mega', 3: 'giga', 4: 'tera'}
@@ -27,10 +23,8 @@ def format_bytes(size):
 
 
 def print_total(bandwidth_used):
-
     formated_bytes, unit = format_bytes(bandwidth_used)
     print(f"Total bandwidth used: {formated_bytes} {unit}", end="\r")
-
 
 if __name__ == '__main__':
     __main__()
